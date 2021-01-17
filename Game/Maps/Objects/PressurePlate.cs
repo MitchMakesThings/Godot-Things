5f5b4ab24e8f867111c1160d89ae1f6dd6b89ca5
Mitch <mitchreidnz@gmail.com>
2021-01-17 17:47:21 +1300

    Changing pressure plates to emit a signal, instead of having a singular activation target like pedestals do

--- Game/Maps/Objects/PressurePlate.cs
+++ Game/Maps/Objects/PressurePlate.cs
@@ -3,27 +3,26 @@ using System;
 
 public class PressurePlate : Spatial
 {
-	[Export]
-	NodePath ActivationNodePath;
+	[Signal]
+	public delegate void OnActivated();
 
-	IActivatable ActivationNode;
+	[Signal]
+	public delegate void OnDeactivated();
 
 	public override void _Ready()
 	{
 		base._Ready();
-
-		ActivationNode = GetNode<IActivatable>(ActivationNodePath);
 	}
 
 	private void _on_Area_body_entered(object body)
 	{
 		// TODO - check type of body, we might only want to trigger for the player etc
-		ActivationNode.Activate();
+		EmitSignal(nameof(OnActivated));
 	}
 
 	private void _on_Area_body_exited(object body)
 	{
 		// TODO - ensure all the bodies that entered have now exited
-		ActivationNode.Deactivate();
+		EmitSignal(nameof(OnDeactivated));
 	}
 }
