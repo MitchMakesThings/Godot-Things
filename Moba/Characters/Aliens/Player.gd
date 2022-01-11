90c4b54efd2540e202c800d004a4738676aae68c
MitchReidNZ <Mitchreidnz@gmail.com>
2022-01-11 19:23:12 +0000

    Initial commit

---
+++ Moba/Characters/Aliens/Player.gd
@@ -0,0 +1,27 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_FORCE = -400.0
+
+# Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		motion_velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		motion_velocity.y = JUMP_FORCE
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		motion_velocity.x = direction * SPEED
+	else:
+		motion_velocity.x = move_toward(motion_velocity.x, 0, SPEED)
+
+	move_and_slide()
