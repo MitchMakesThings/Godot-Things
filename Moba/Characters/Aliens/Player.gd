25aa010d3a9f714d38e0451886e800027a8e3cb3
MitchReidNZ <Mitchreidnz@gmail.com>
2022-01-23 12:10:35 +0000

    Fixed up and tested. Player movement is server authoritative and synced!

--- Moba/Characters/Aliens/Player.gd
+++ Moba/Characters/Aliens/Player.gd
@@ -1,13 +1,25 @@
 extends CharacterBody2D
 
-const SPEED = 600.0
-const JUMP_FORCE = -600.0
+const SPEED = 300.0
+const JUMP_FORCE = -400.0
 
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+var sync_position : Vector2
+
+func _ready():
+	$Camera2D.current = is_local_authority()
+
+func is_local_authority() -> bool:
+	return name == str(multiplayer.get_unique_id())
 
 func _physics_process(delta):
+	if !is_local_authority():
+		# TODO lerp to sync_position
+		position = sync_position
+		return
+
 	# Add the gravity.
 	if not is_on_floor():
 		motion_velocity.y += gravity * delta
@@ -23,5 +35,17 @@ func _physics_process(delta):
 		motion_velocity.x = direction * SPEED
 	else:
 		motion_velocity.x = move_toward(motion_velocity.x, 0, SPEED)
-
+	
+	# Move locally
 	move_and_slide()
+	
+	# Sync position to the server
+	rpc_id(1, StringName('push_to_server'), position)
+
+@rpc(any_peer, unreliable_ordered)
+func push_to_server(newPosition : Vector2):
+	# Validate!
+	if name != str(multiplayer.get_remote_sender_id()):
+		print('someone being naughty! ', multiplayer.get_remote_sender_id(), ' tried to update ', name)
+		return
+	sync_position = newPosition
