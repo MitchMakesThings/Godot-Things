d1ce4244ae051ee1306e97bf29939db0a27b1bde
MitchReidNZ <Mitchreidnz@gmail.com>
2022-02-07 19:57:14 +0000

    Giving the button a reset timer, so it's not pressed early on and forgotten until the server restarts

--- Networking/Advanced/Multiplayer/Items/blue_button.gd
+++ Networking/Advanced/Multiplayer/Items/blue_button.gd
@@ -2,11 +2,17 @@ extends Area2D
 
 @export
 var pressed_texture : Texture2D
+@export
+var default_texture : Texture2D
+@export
+var reset_delay := 3.0
 
 var isPressed : bool:
 	set (newValue):
 		if newValue:
 			$Sprite2D.texture = pressed_texture
+		else:
+			$Sprite2D.texture = default_texture
 		isPressed = newValue
 
 func _on_blue_button_body_entered(body):
@@ -15,3 +21,6 @@ func _on_blue_button_body_entered(body):
 	if not body is Player:
 		return
 	isPressed = true
+	if multiplayer.is_server():
+		await get_tree().create_timer(reset_delay).timeout
+		isPressed = false
