a9792123e6ed4b85eb11f815a8254ecf6d6a88af
MitchReidNZ <mitchreidnz@gmail.com>
2022-02-14 20:18:03 +0000

    Refactoring networking code away from the example level

---
+++ Networking/Advanced/Multiplayer/System/main.gd
@@ -0,0 +1,42 @@
+extends Node
+
+@export
+var PlayerScene = preload("res://Characters/Aliens/Player.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Start the server if Godot is passed the "--server" argument,
+	# and start a client otherwise.
+	if "--server" in OS.get_cmdline_args():
+		start_network(true)
+	else:
+		start_network(false)
+
+func start_network(server: bool) -> void:
+	var peer = ENetMultiplayerPeer.new()
+	if server:
+		# Listen to peer connections, and create new player for them
+		multiplayer.peer_connected.connect(self.create_player)
+		# Listen to peer disconnections, and destroy their players
+		multiplayer.peer_disconnected.connect(self.destroy_player)
+		
+		peer.create_server(4242)
+		print('server listening on localhost 4242')
+	else:
+		peer.create_client("localhost", 4242)
+
+	multiplayer.set_multiplayer_peer(peer)
+
+func create_player(id : int) -> void:
+	# Instantiate a new player for this client.
+	var p = PlayerScene.instantiate()
+
+	# Set the name, so players can figure out their local authority
+	p.name = str(id)
+	
+	print('added player: ', str(id), ' owned by: ', p.get_multiplayer_authority())
+	$Players.add_child(p)
+
+func destroy_player(id : int) -> void:
+	# Delete this peer's node.
+	$Players.get_node(str(id)).queue_free()
