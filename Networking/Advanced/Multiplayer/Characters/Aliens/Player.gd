f7263ea003767c9f67afec8c249387376688740b
MitchReidNZ <mitchreidnz@gmail.com>
2022-02-20 17:41:13 +0000

    Moving networked variables to a child node, to minimise the spread of network authority, per discussion with Faless. Note: This relies on a custom Godot build, due to path-only sync bug with Alpha 2 (https://github.com/godotengine/godot/issues/58193)

--- Networking/Advanced/Multiplayer/Characters/Aliens/Player.gd
+++ Networking/Advanced/Multiplayer/Characters/Aliens/Player.gd
@@ -11,43 +11,43 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var jump_fuel := MAX_JUMP_FUEL
 
-var sync_position : Vector2:
+var is_jumping : bool:
 	set(value):
-		sync_position = value
-		if not is_multiplayer_authority():
-			position = value
-
-var sync_motion_velocity : Vector2
-var sync_is_jumping : bool:
-	set(value):
-		sync_is_jumping = value
-		if not is_multiplayer_authority():
+		is_jumping = value
+		if not is_local_authority():
 			# Note, this is a CPU Particles node!
 			# Godot 4 currently has a bug where GPU Particles in global space are incorrectly offset
 			# https://github.com/godotengine/godot/issues/56892
 			$JetpackParticles.emitting = value
 
+func is_local_authority():
+	return $Networking/MultiplayerSynchronizer.get_multiplayer_authority() == multiplayer.get_unique_id()
+
 func _ready():
 	# int constructor taking a string is currently broken :(
 	# https://github.com/godotengine/godot/issues/44407
 	# https://github.com/godotengine/godot/issues/55284
-	set_multiplayer_authority(str(name).to_int())
+	$Networking/MultiplayerSynchronizer.set_multiplayer_authority(str(name).to_int())
 	
-	$Camera2D.current = is_multiplayer_authority()
-	$UI.visible = is_multiplayer_authority()
+	$Camera2D.current = is_local_authority()
+	$UI.visible = is_local_authority()
 
 func _process(_delta):
 	$UI/TextureProgressBar.value = jump_fuel
 	$UI/TextureProgressBar.visible = jump_fuel < MAX_JUMP_FUEL
 
 func _physics_process(delta):
-	if !is_multiplayer_authority():
+	if !is_local_authority():
 		# TODO look at these numbers in depth!
 		# We're trying to smoothly sync the local position of other clients to the server
-		if position.distance_squared_to(sync_position) > 100:
-			position = position.lerp(sync_position, 0.8)
-			print(position.distance_squared_to(sync_position))
-		motion_velocity = sync_motion_velocity
+		if position.distance_squared_to($Networking.sync_position) > 100:
+			position = position.lerp($Networking.sync_position, 0.8)
+			print(position.distance_squared_to($Networking.sync_position))
+		motion_velocity = $Networking.sync_motion_velocity
+		
+		# Update our local is_jumping, to control jetpack particles
+		is_jumping = $Networking.sync_is_jumping
+		
 		move_and_slide()
 		return
 
@@ -59,17 +59,17 @@ func _physics_process(delta):
 	if Input.is_action_pressed("jump") and jump_fuel >= 0:
 		motion_velocity.y = JUMP_FORCE
 		jump_fuel -= 1
-		sync_is_jumping = true
-		# If we drive particles based on sync_is_jumping, our locally written value
+		is_jumping = true
+		# If we drive particles based on is_jumping, our locally written value
 		# gets overridden on the next packet from the server.
 		# So we'll handle our particles locally, 
-		# and let sync_is_jumping manage the other clients.
+		# and let is_jumping manage the other clients.
 		$JetpackParticles.emitting = true
 		
 		if $Camera2D.zoom.length() < CAMERA_MAX_ZOOM.length():
 			$Camera2D.zoom = $Camera2D.zoom.lerp(CAMERA_MAX_ZOOM, 0.01)
 	else:
-		sync_is_jumping = false
+		is_jumping = false
 		$JetpackParticles.emitting = false
 		var zoom = $Camera2D.zoom.length()
 		if zoom > 1:
@@ -84,19 +84,11 @@ func _physics_process(delta):
 		motion_velocity.x = direction * SPEED
 	else:
 		motion_velocity.x = move_toward(motion_velocity.x, 0, SPEED)
-	
-	# Sync position to the server
-	rpc_id(1, StringName('push_to_server'), position, motion_velocity, sync_is_jumping)
-	
+
 	# Move locally
 	move_and_slide()
-
-@rpc(unreliable_ordered)
-func push_to_server(newPosition : Vector2, motion : Vector2, is_jumping : bool):
-	# Validate!
-	if not multiplayer.is_server():
-		print('someone being naughty! ', multiplayer.get_remote_sender_id(), ' tried to update ', name)
-		return
-	sync_position = newPosition
-	sync_is_jumping = is_jumping
-	sync_motion_velocity = motion
+	
+	# Update sync variables, which will be replicated to everyone else
+	$Networking.sync_position = position
+	$Networking.sync_motion_velocity = motion_velocity
+	$Networking.sync_is_jumping = is_jumping
---
+++ Networking/Advanced/Multiplayer/Characters/Aliens/player_networking.gd
@@ -0,0 +1,5 @@
+extends Node
+
+var sync_position : Vector2
+var sync_motion_velocity : Vector2
+var sync_is_jumping : bool
