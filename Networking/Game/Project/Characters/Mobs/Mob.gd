265fd91c12122fe86047d8ba7aad816ee33b8c05
MitchMakesThings <mitchreidnz@gmail.com>
2023-02-05 21:08:32 +0000

    Randomizing mob destination
    Also speeding up the mob spawn rate, and adding a bunch of comments

--- Networking/Game/Project/Characters/Mobs/Mob.gd
+++ Networking/Game/Project/Characters/Mobs/Mob.gd
@@ -1,19 +1,35 @@
 extends CharacterBody2D
 class_name Mob
 
-var SPEED := 80
+var SPEED := 100
 var velocity_sync : Vector2
 var global_pos_sync : Vector2
 var is_dead := false
 
+@export
+var targets : Array[Vector2] = []
+var target_sync : Vector2
+
 var health_sync := 3:
+	# We're using a setget variable here. It's updated by the MultiplayerSynchronizer
+	# This way when the server broadcasts a change in health all clients can show the damage flash animation.
 	set (value):
+		# Because MultiplayerSynchronizer sends the health_sync value ~60 times a second
+		# (See MultiplayerSynchronizer nodes Replication Interval)
+		# We need to check to see if it's actually changed before flashing damage!
+		# An alternative (better!) approach would be to make flash_damage an @rpc
+		# Then the server could call flash_damage when the health changes. All clients would show the animation.
+		# But I thought it was good to show off how you could use a setget variable :)
 		var has_changed = value != health_sync
 		health_sync = value
 		if has_changed:
 			flash_damage()
 	get:
 		return health_sync
+		
+func _ready():
+	# Remember: _ready() runs on _all_ computers - server & all clients
+	pick_new_target()
 
 func _physics_process(_delta : float):
 	if not multiplayer.is_server():
@@ -26,15 +42,32 @@ func _physics_process(_delta : float):
 		velocity_sync = Vector2.ZERO
 		return
 	
-	# TODO select a target location from the list, and try to move there
-	var direction := (Vector2(1618, -1455) - global_position).normalized()
-	
-	velocity = direction * SPEED
+	# Move in a straight line towards our target
+	# Note that we're not pathfinding, so we can't work our way around obstacles.
+	# You can see this in the example scene, where mobs will slide along obstacles.
+	var direction := (target_sync - global_position)
+	velocity = direction.normalized() * SPEED
 	move_and_slide()
 	velocity_sync = velocity
 	global_pos_sync = global_position
+	
+	# If we've moved close enough to our target destination pick a new one
+	# This would let you implement guard waypoint pathing etc.
+	# This size should be adjusted based on the size of your mobs
+	if direction.length() <= 80:
+		pick_new_target()
+
+func pick_new_target() -> void:
+	# We only want the server to pick a target. It'll then sync with all the clients.
+	# If we didn't check that we were the server different clients could pick different targets
+	# Since we're syncing position directly, it wouldn't cause problems in this example
+	# But it might catch you out in your projects!
+	if multiplayer.is_server():
+		target_sync = targets[randi() % len(targets)]
 
 func take_damage(amount : int):
+	# Note that our projectile always calls take_damage, on the server & all clients
+	# We only want to process damage on the server, since it's our source of truth.
 	if multiplayer.is_server():
 		health_sync -= amount
 
@@ -43,7 +76,6 @@ func flash_damage():
 	tween.tween_property($Sprite2D, "modulate", Color.RED, 0.1)
 	tween.tween_property($Sprite2D, "modulate", Color.WHITE, 0.3)
 	
-	# TODO explode in a cloud of red particles, then queue_free once they're done
 	if health_sync <= 0:
 		# Fade out of visibility and trigger blood particles
 		tween.tween_property($Sprite2D, "modulate", Color("ffffff00"), 0.1)
--- Networking/Game/Project/System/main.gd
+++ Networking/Game/Project/System/main.gd
@@ -6,6 +6,13 @@ var PlayerScene = preload("res://Characters/Aliens/Character.tscn")
 @export
 var MobScene = preload("res://Characters/Mobs/mob.tscn")
 
+# This should really be configured on the TestMap (and other maps!)
+# That way individual maps could specify where mobs move to/from
+# This script could then get those by querying a Map scripts mob_targets property.
+# For now, I'm being lazy :)
+@export
+var mob_targets : Array[Vector2] = []
+
 func _ready():
 	if NetworkState.is_server:
 		# Listen to peer connections, and create new character for them
@@ -27,9 +34,14 @@ func destroy_player(id : int) -> void:
 	$Players.get_node(str(id)).queue_free()
 
 
+# Called from the Timer node
 func _on_mob_spawn_timeout():
 	if not multiplayer.is_server():
 		return
+	# Limit the scene to 100 whales. 
+	# This is to keep the screen from becoming too cluttered (and because we're not using pathfinding!)
+	# Not a performance concern.
 	if len(get_tree().get_nodes_in_group("mobs")) < 100:
 		var mob = MobScene.instantiate()
+		mob.targets = mob_targets
 		get_node("Mobs").add_child(mob, true)
